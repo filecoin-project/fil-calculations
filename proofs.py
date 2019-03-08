1d211b55b79d4c167e92470b21833b36070821e1
porcuquine <porcuquine@users.noreply.github.com>
2019-03-07 18:37:33 -0800

    First (bad) approximation of proving-time sector-size justification.

--- proofs.py
+++ proofs.py
@@ -49,40 +49,6 @@ class Security(object):
 # FIXME: What is the exact real number of challenges?
 filecoin_security_requirements = Security(base_degree=5, expansion_degree=8, layers=10, sloth_iter=0, total_challenges=8848)
 
-# TODO: Make it so this can be extracted directly (and correctly) from the JSON results of the zigzag example.
-class Instance(object):
-    """Concrete implementations with known benchmarks of fixed parameters on a specific machine."""
-    def __init__(self, **kwargs):
-        self.security = kwargs.get('security', filecoin_security_requirements) # Assume we are using secure params if not otherwise specified.
-        self.replication_time_per_GiB = kwargs.get('replication_time') # vcpu-seconds
-        # Is reported vanilla proving time serial, or do we have to account for parallelism?
-        self.vanilla_proving_time = kwargs.get('vanilla_proving_time') # vcpu-seconds
-        self.constraints = kwargs.get('constraints')
-        self.groth_proving_time = kwargs.get('groth_proving_time') # vcpu-seconds
-        self.proving_time_per_constraint = self.groth_proving_time / self.constraints
-
-# ➜  rust-proofs git:(zigzag-example-taper) ✗ ./target/release/examples/zigzag --m 5 --expansion 8 --layers 10 --challenges 5 --size 262144 --groth
-# Feb 22 22:47:42.385 INFO replication_time/GiB: 2588.454166843s, target: stats, place: filecoin-proofs/examples/zigzag.rs:176 zigzag, root: filecoin-proofs
-# Feb 22 22:49:03.378 INFO vanilla_proving_time: 80.99321579 seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:208 zigzag, root: filecoin-proofs
-# Feb 22 22:51:34.656 INFO circuit_num_constraints: 31490555, target: stats, place: filecoin-proofs/examples/zigzag.rs:255 zigzag, root: filecoin-proofs
-# Feb 22 22:55:23.362 INFO groth_parameter_bytes: 13777652280, target: stats, place: storage-proofs/src/parameter_cache.rs:131 storage_proofs::parameter_cache, root: storage-proofs
-# Feb 22 22:57:57.967 INFO groth_proving_time: 310.895328636s seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:272 zigzag, root: filecoin-proofs
-# real wall clock proving time = 22:57:57 - 22:55:23 = 2:34 = 154s
-# vcpu-seconds = 154s * 28 vcpus = 4312
-# FIXME: replication_time here and below is only serial time. It ignores parallel merkle-tree-generation time.
-porcuquine_prover = Instance(replication_time=2588, constraints=31490555, groth_proving_time=4312, vanilla_proving_time=80.99)
-
-# [ec2-user@ip-172-31-47-121 rust-fil-proofs]$ ./target/release/examples/zigzag --m 5 --expansion 8 --layers 10 --challenges 333 --taper-layers 7 --taper 0.3 --size 262144 --groth --no-bench --partitions 8
-# Feb 28 09:58:40.228 INFO replication_time/GiB: 3197.191021367s, target: stats, place: filecoin-proofs/examples/zigzag.rs:178 zigzag, root: filecoin-proofs
-# Feb 28 10:53:37.132 INFO vanilla_proving_time: 3296.904225516 seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:210 zigzag, root: filecoin-proofs
-# Feb 28 13:06:22.781 INFO groth_parameter_bytes: 336470126136, target: stats, place: storage-proofs/src/parameter_cache.rs:164 storage_proofs::parameter_cache, root: storage-proofs
-# Feb 28 20:48:29.006 INFO groth_proving_time: 34734.387995685s seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:274 zigzag, root: filecoin-proofs
-# real wall clock proving time = 20:48:29 - 13:06:22 = 7:42:07 = 27727s
-# vcpu-seconds = 27727s * 128 vcpus = 3549056
-ec2_x1e32_xlarge = Instance(replication_time=3197, constraints=5572568613, # FIXME: constraints is a projection -- bench the real value.
-                            groth_proving_time=3549056,
-                            vanilla_proving_time=3297)
-
 class HashFunction(object):
     ## For now, assume 64 byte input, 32 byte output
     def __init__(self, time, constraints):
@@ -95,6 +61,56 @@ class HashFunction(object):
 pedersen = HashFunction(0.000017993, 1324)
 blake2s = HashFunction(1.0428e-7, 10324)
 
+class MerkleTree(object):
+    def __init__(self, nodes, hash_function):
+        self.nodes = nodes
+        self.hash_function = hash_function
+        self.height = math.ceil(math.log2(nodes)) + 1
+
+    # Assuming no parallelism
+    def time(self):
+        return self.hash_function.time() * (self.nodes - 1)
+
+# TODO: Make it so this can be extracted directly (and correctly) from the JSON results of the zigzag example.
+class Instance(object):
+    """Concrete implementations with known benchmarks of fixed parameters on a specific machine."""
+    def __init__(self, **kwargs):
+        self.security = kwargs.get('security', filecoin_security_requirements) # Assume we are using secure params if not otherwise specified.
+        self.encoding_replication_time_per_GiB = kwargs.get('encoding_replication_time_per_GiB') # vcpu-seconds
+        # Is reported vanilla proving time serial, or do we have to account for parallelism?
+        self.vanilla_proving_time = kwargs.get('vanilla_proving_time') # vcpu-seconds
+        self.constraints = kwargs.get('constraints')
+        self.groth_proving_time = kwargs.get('groth_proving_time') # vcpu-seconds
+        self.proving_time_per_constraint = self.groth_proving_time / self.constraints
+
+    def merkle_tree_replication_time_per_GiB(self):
+        # For now, assume merkle tree uses pedersen hashes.
+        return MerkleTree(GiB / 32, pedersen).time() * 11  # Grrr... hard-coding layers, needs to move to ZigZag in refactor.
+
+    def replication_time_per_GiB(self):
+        return self.encoding_replication_time_per_GiB + self.merkle_tree_replication_time_per_GiB()
+
+# ➜  rust-proofs git:(zigzag-example-taper) ✗ ./target/release/examples/zigzag --m 5 --expansion 8 --layers 10 --challenges 5 --size 262144 --groth
+# Feb 22 22:47:42.385 INFO replication_time/GiB: 2588.454166843s, target: stats, place: filecoin-proofs/examples/zigzag.rs:176 zigzag, root: filecoin-proofs
+# Feb 22 22:49:03.378 INFO vanilla_proving_time: 80.99321579 seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:208 zigzag, root: filecoin-proofs
+# Feb 22 22:51:34.656 INFO circuit_num_constraints: 31490555, target: stats, place: filecoin-proofs/examples/zigzag.rs:255 zigzag, root: filecoin-proofs
+# Feb 22 22:55:23.362 INFO groth_parameter_bytes: 13777652280, target: stats, place: storage-proofs/src/parameter_cache.rs:131 storage_proofs::parameter_cache, root: storage-proofs
+# Feb 22 22:57:57.967 INFO groth_proving_time: 310.895328636s seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:272 zigzag, root: filecoin-proofs
+# real wall clock proving time = 22:57:57 - 22:55:23 = 2:34 = 154s
+# vcpu-seconds = 154s * 28 vcpus = 4312
+# FIXME: replication_time here and below is only serial time. It ignores parallel merkle-tree-generation time. Assumes (safely) that was the bottleneck in measured time.
+porcuquine_prover = Instance(encoding_replication_time_per_GiB=2588, constraints=31490555, groth_proving_time=4312, vanilla_proving_time=80.99)
+
+# [ec2-user@ip-172-31-47-121 rust-fil-proofs]$ ./target/release/examples/zigzag --m 5 --expansion 8 --layers 10 --challenges 333 --taper-layers 7 --taper 0.3 --size 262144 --groth --no-bench --partitions 8
+# Feb 28 09:58:40.228 INFO replication_time/GiB: 3197.191021367s, target: stats, place: filecoin-proofs/examples/zigzag.rs:178 zigzag, root: filecoin-proofs
+# Feb 28 10:53:37.132 INFO vanilla_proving_time: 3296.904225516 seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:210 zigzag, root: filecoin-proofs
+# Feb 28 13:06:22.781 INFO groth_parameter_bytes: 336470126136, target: stats, place: storage-proofs/src/parameter_cache.rs:164 storage_proofs::parameter_cache, root: storage-proofs
+# Feb 28 20:48:29.006 INFO groth_proving_time: 34734.387995685s seconds, target: stats, place: filecoin-proofs/examples/zigzag.rs:274 zigzag, root: filecoin-proofs
+# real wall clock proving time = 20:48:29 - 13:06:22 = 7:42:07 = 27727s
+# vcpu-seconds = 27727s * 128 vcpus = 3549056
+ec2_x1e32_xlarge = Instance(encoding_replication_time_per_GiB=3197, constraints=5572568613, # FIXME: constraints is a projection, bench the real value.
+                            groth_proving_time=3549056,
+                            vanilla_proving_time=3297)
 
 class ZigZag(object):
     """ZigZag Model"""
@@ -142,10 +158,10 @@ class ZigZag(object):
 
     def total_replication_time(self):
         if self.instance:
-            return self.instance.replication_time_per_GiB * (self.sector_size / GiB)
+            return self.instance.replication_time_per_GiB() * (self.sector_size / GiB)
         else:
             # Calculate a projection, as in the calculator.
-            assert false, unimplemented
+            assert false, "unimplemented"
 
     def vanilla_proving_time(self):
         if self.instance:
@@ -180,19 +196,7 @@ class ZigZag(object):
 
 #    def seal_time_for_sector_size(self, sector_size):
 
-
-
 #    def replication_time(self, machine):
-
-class MerkleTree(object):
-    def __init__(self, nodes, hash_function):
-        self.nodes = nodes
-        self.hash_function = hash_function
-        self.height = math.ceil(math.log2(nodes)) + 1
-
-    # Assuming no parallelism
-    def time(self):
-        return self.hash_function.time() * (self.nodes - 1)
 
 class Machine(object):
     """Machine Model"""
@@ -224,6 +228,3 @@ c = Config(z, m)
 p = z.performance()
 
 print(f"ZigZag nodes: {z.nodes()}")
-
-
-
